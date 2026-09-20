49c9630a579735963a29b4890e5eac890fcb5ea8
Joan Cubillo Arguedas <joan.cubillo95@gmail.com>
2026-09-19 20:15:31 -0600

    Add simple movie controller

---
+++ src/main/java/com/joancubillo/moviesapi/movie/MovieController.java
@@ -0,0 +1,19 @@
+package com.joancubillo.moviesapi.movie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @GetMapping("/getAll")
+    public List<Movie> hello(){
+        return movieRepository.findAll();
+    }
+}
